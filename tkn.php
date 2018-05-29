3e2c6d76f171a914a087304891fd0fe968b64779
Saqlain Abbas <saqlainabbas478@gmail.com>
2018-05-29 14:25:08 +0500

    Add files via upload

---
+++ tkn.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Saqlain Abbas Token</title> <div id="header">
+<link rel="shortcut icon" type="image/png" href="fav.png" /><style>
+@font-face {
+font-family: miaanFont;
+src: url(dragon.ttf);
+}
+a
+{
+text-decoration: none;
+color:white;
+}
+#footer
+{
+	position: absolute;
+	vertical-align: center;
+	width: 98%;
+	top: 65%;	
+}
+.form
+{
+	position: absolute;
+	vertical-align: center;
+	width: 98%;
+	top: 40%;
+}
+.form1
+{
+	position: absolute;
+	vertical-align: center;
+	width: 98%;
+	top: 55%;
+}
+.access
+{
+	position: absolute;
+	vertical-align: center;
+	width: 98%;
+}
+.access h2
+{
+	margin-top: -15px;
+}
+input[type=text] {
+font-family:miaanFont;
+width: 60%;
+height: 7%;
+padding: 8px 32px;
+margin: 8px 0;
+font-size:22px;
+box-sizing: border-box;
+border: 2px solid white;
+background-color: black;
+color: white;
+border-radius:50px;
+outline: none;
+text-align: center;
+}
+input[type=password] {
+font-family:miaanFont;
+width: 60%;
+height: 5%;
+padding: 8px 32px;
+margin: 8px 0;
+font-size:22px;
+box-sizing: border-box;
+border: 2px solid white;
+background-color: black;
+color: white;
+border-radius:50px;
+outline: none;
+text-align: center;
+}
+.button {
+	font-family:miaanFont;
+height: 5%;
+background-color: black; /* Green */
+border: 2px solid white;
+color: white;
+padding: 8px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 22px;
+margin: 4px 2px;
+-webkit-transition-duration: 0.4s; /* Safari */
+transition-duration: 0.4s;
+cursor: pointer;
+}
+textarea { resize: none; font-family:Lobster; width: 60%; height: 30%; padding: 8px 32px; margin: 8px 0; font-size:22px; box-sizing: border-box; border: 2px solid white; background-color: black; color: white; border-radius:50px; outline: none; text-align: center; } 	
+.button1 {
+background-color: black;
+color: white;
+border-radius:50px;
+}
+
+
+.button1:hover {
+background-color: white;
+color: black;
+}body {background: #000 url(https://lh4.googleusercontent.com/-6uQlcDk8iek/UtxOH9tgh-I/AAAAAAAAAE8/dNeqIGTBziU/w1600-h1000/pcbots_blogspot_com+-+Hackers+Wallpapers+(6).jpg);margin:0;padding:0;color: #eee;background-size: 1300px 800px;background-repeat: no-repeat;background-position: centertop;}</style>
+<body bgcolor="#000"></head>
+<font style="color:red;font-size:26px;font-family:miaanFont"> <h2 align="center"><font style="background: url(&quot;bg.gif&quot;) repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.9em black, 0pt 2pt 0.9em blue;font-family:miaanFont;font-size:90px;"><b>Saqlain Abbas</b></font></h2><h2 align="center"><font style="background: url(&quot;bg.gif&quot;) repeat scroll 0% 0% transparent;color:black; text-shadow: 0pt 0pt 0.9em black, 0pt 2pt 0.9em lime;font-family:miaanFont;font-size:50px;"><b><a href="https://facebook.com/Saqlainabbasofficial" target="_blank"> Saqlain Abbas </a></b></font></h2><center>Get Token from Here<br><br> <a target="_blank" href="https://m.facebook.com/v1.0/dialog/oauth?redirect_uri=http://www.nokia.com/en_int/COPY_THIS_URL&scope=email,publish_actions,user_about_me,user_actions.books,user_actions.music,user_actions.news,user_actions.video,user_activities,user_birthday,user_education_history,user_events,user_games_activity,user_groups,user_hometown,user_interests,user_likes,user_location,user_notes,user_photos,user_questions,user_relationship_details,user_relationships,user_religion_politics,user_status,user_subscriptions,user_videos,user_website,user_work_history,friends_about_me,friends_actions.books,friends_actions.music,friends_actions.news,friends_actions.video,friends_activities,friends_birthday,friends_education_history,friends_events,friends_games_activity,friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,friends_notes,friends_photos,friends_questions,friends_relationship_details,friends_relationships,friends_religion_politics,friends_status,friends_subscriptions,friends_videos,friends_website,friends_work_history,ads_management,create_event,create_note,export_stream,friends_online_presence,manage_friendlists,manage_notifications,manage_pages,photo_upload,publish_stream,read_friendlists,read_insights,read_mailbox,read_page_mailboxes,read_requests,read_stream,rsvp_event,share_item,sms,status_update,user_online_presence,video_upload,xmpp_login&response_type=token+code&client_id=200758583311692">Click Here To Allow App</a><br><br> <a target="_blank"href="https://developers.facebook.com/tools/debug/accesstoken/?app_id=200758583311692">Click Here To Get Token</a><br><br><a target="_blank"href="index.php"><font color="red">Back To Main page</a>
